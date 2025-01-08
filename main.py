806d1ff7772c7ba6bb645f97ceb1549f8d8687f5
cimini-xyz <github@cimini.xyz>
2025-01-07 19:51:36 -0600

    Added course provided asteroid and asteroid field code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -13,10 +15,15 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    asteroidfield = AsteroidField()
+    
     while True:
         #logic  
         for event in pygame.event.get():
