afbd834093dccdf5ad62c8b309573aa64f615209
cimini-xyz <github@cimini.xyz>
2025-01-10 10:20:26 -0600

    Refactored config entries and data from manager to config, added comments for manager methods

--- vfx/ghostimage/config.py
+++ vfx/ghostimage/config.py
@@ -1,7 +1,21 @@
+from collections import defaultdict
+
 from draw.none import draw_none
+from draw.triangle import draw_triangle
+from draw.circle import draw_circle
 
+from player import Player
+from shot import Shot
+from constants import (
+    PLAYER_GHOST_IMAGE_FREQUENCY,
+    PLAYER_GHOST_IMAGE_LENGTH,
+    PLAYER_GHOST_IMAGE_INTENSITY,
+    SHOT_GHOST_IMAGE_FREQUENCY,
+    SHOT_GHOST_IMAGE_LENGTH,
+    SHOT_GHOST_IMAGE_INTENSITY
+)
 
-class GhostImageConfig():
+class GhostImageConfigEntry():
     def __init__(
             self,
             draw_function=draw_none,
@@ -13,3 +27,23 @@ class GhostImageConfig():
         self.frequency = frequency
         self.length = length
         self.intensity = intensity
+
+class GhostImageConfig():
+    entries = defaultdict(
+        GhostImageConfigEntry,
+        {
+            Player : GhostImageConfigEntry(
+                draw_triangle,
+                PLAYER_GHOST_IMAGE_FREQUENCY,
+                PLAYER_GHOST_IMAGE_LENGTH,
+                PLAYER_GHOST_IMAGE_INTENSITY
+            ),
+            Shot : GhostImageConfigEntry(
+                draw_circle,
+                SHOT_GHOST_IMAGE_FREQUENCY,
+                SHOT_GHOST_IMAGE_LENGTH,
+                SHOT_GHOST_IMAGE_INTENSITY
+            )
+        }
+    )
+    
--- vfx/ghostimage/manager.py
+++ vfx/ghostimage/manager.py
@@ -1,89 +1,74 @@
-from vfx.ghostimage.emitter import GhostImageEmitter
 from vfx.ghostimage.config import GhostImageConfig
 from vfx.ghostimage.source import GhostImageSource
-
-from collections import defaultdict
-
-from player import Player
-from shot import Shot
-
-
-from draw.triangle import draw_triangle
-from draw.circle import draw_circle
-
-from constants import (
-    PLAYER_GHOST_IMAGE_FREQUENCY,
-    PLAYER_GHOST_IMAGE_LENGTH,
-    PLAYER_GHOST_IMAGE_INTENSITY,
-    SHOT_GHOST_IMAGE_FREQUENCY,
-    SHOT_GHOST_IMAGE_LENGTH,
-    SHOT_GHOST_IMAGE_INTENSITY
-)
 
 
 class GhostImageManager():
     def __init__(self, sprite_group, emitter):
         self.emitter = emitter
         self.sprite_group = sprite_group
-
-        self.config = defaultdict(GhostImageConfig)
-        self.config[Player] = GhostImageConfig(
-            draw_triangle,
-            PLAYER_GHOST_IMAGE_FREQUENCY,
-            PLAYER_GHOST_IMAGE_LENGTH,
-            PLAYER_GHOST_IMAGE_INTENSITY
-        )
-        self.config[Shot] = GhostImageConfig(
-            draw_circle,
-            SHOT_GHOST_IMAGE_FREQUENCY,
-            SHOT_GHOST_IMAGE_LENGTH,
-            SHOT_GHOST_IMAGE_INTENSITY
-        )
-
         self.registered_sprites = []
         self.emission_sources = []
-
+    
+    # Main loop function
     def update(self, dt):
         self.unregister_dead_sprites()
         self.register_alive_sprites()
         self.increment_emission_timers(dt)
         self.spawn_emissions()
 
+    # Getter
     def get_ready_emission_sources(self):
+        """Returns all emission sources that are ready to generate an emission
+        
+        Returns:
+            Generator of GhostImageSource objects whose timer has exceeded their frequency
+        """
         return (
             source for source
             in self.emission_sources
             if source.last_emission_time >= source.frequency
         )
 
+    # Registration methods
+    # Base sprite registration
     def register_sprite(self, sprite):
+        """Primary method for registering a single sprite and generating its emission source"""
         self.registered_sprites.append(sprite)
         self.emission_sources.append(
             GhostImageSource(
                 sprite,
-                self.config[type(sprite)]
+                GhostImageConfig.entries[type(sprite)]
             )
         )
 
+    # Register all unregistered sprites
     def register_alive_sprites(self):
+        """Registers all unregistered sprites that exist in sprite_group alongside their respective emission sources"""
         for sprite in self.sprite_group:
             if sprite not in self.registered_sprites:
                 self.register_sprite(sprite)
 
+    # Base sprite removal from registry
     def unregister_sprite(self, source):
+        """Primary method to remove sprite and its emission source from registry"""
         self.registered_sprites.remove(source.sprite)
         self.emission_sources.remove(source)
 
+    # Unregister all sprites that no longer exist
     def unregister_dead_sprites(self):
+        """Unregisters sprites and its emission sources that no longer exist in their sprite group"""
         for source in self.emission_sources:
             if source.sprite not in self.sprite_group:
                 self.unregister_sprite(source)
 
+    # Emission creation and modification
     def increment_emission_timers(self, dt):
+        """Increases all emission timers by delta time"""
         for source in self.emission_sources:
             source.increment_last_emission_time(dt)
-            
+
     def spawn_emissions(self):
+        """If emission timers pass their frequency threshold, generate an emission and reset timer"""
         for source in self.get_ready_emission_sources():
             self.emitter.register_emission(
                 source.create_emission()
