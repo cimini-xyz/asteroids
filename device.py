236421268e02b65915252f2951e6be46d53746d5
cimini-xyz <github@cimini.xyz>
2025-01-12 14:07:16 -0600

    Added support for bus routing for all devices, bus name is arbitrary identifier, default bus_name is None

--- device.py
+++ device.py
@@ -1,11 +1,14 @@
 from enum import Enum, auto
-from collections import deque
+from collections import deque, defaultdict
+from itertools import chain
 
 def clamp(value, min_value=0, max_value=1.0):
     return max(min_value, min(max_value, value))
 
 def constant(value):
-    return lambda t: value
+    def func():
+        return value
+    return func
 
 class DeviceState(Enum):
     ALIVE = auto()
@@ -13,24 +16,36 @@ class DeviceState(Enum):
     DISABLED = auto()
     DEAD = auto()
 
+class ConnectionMapping():
+    def __init__(self):
+        self.input = defaultdict(set)
+        self.output = defaultdict(set)
+
+class DeviceMapping():
+    def __init__(self):
+        self.input = defaultdict(set)
+        self.output = defaultdict(set)
+
 class Device:
     def __init__(self, signal_combiner=sum):
         self.state = DeviceState.ALIVE
-        self.output_connections = []
-        self.input_connections = []
+        self.connect = ConnectionMapping()
+        self.device_map = DeviceMapping()
         self.signal_combiner = signal_combiner
-
+        
     def update(self, dt):
         if self.not_updateable():
             return
         return self._update(dt)
 
-    def evaluate(self, t):
-        if self.state in (DeviceState.DISABLED, DeviceState.DEAD):
+    def evaluate(self, bus_name=None):
+        if self.state is DeviceState.DEAD:
             return 0
-        if not isinstance(self, Generator) and not self.input_connections:
+        if not isinstance(self, Generator) and not self.connect.input:
             return 0
-        return self._evaluate(t)
+        if self.state is DeviceState.DISABLED:
+            return sum(self.get_signals(bus_name))
+        return self._evaluate(bus_name)
     
     def not_updateable(self):
         return self.state in (DeviceState.FROZEN, DeviceState.DISABLED, DeviceState.DEAD)
@@ -38,29 +53,43 @@ class Device:
     def _update(self, dt):
         raise NotImplementedError
     
-    def _evaluate(self, t):
+    def _evaluate(self, bus_name=None):
         raise NotImplementedError
     
+    def disconect_all_devices_from_device_map(self, device_map):
+        for connected_device, bus_names in list(device_map.items()):
+            for bus_name in bus_names:
+                connected_device.disconnect_from(self, bus_name)
+
     def remove(self):
-        for output_device in self.output_connections.copy():
-            output_device.input_connections.remove(self)
-        for input_device in self.input_connections.copy():
-            input_device.output_connections.remove(self)
+        self.disconnect_all_devices_from_device_map(self.device_map.input)
+        self.disconnect_all_devices_from_device_map(self.device_map.output)
 
-    def connect_to(self, output_device):
-        if output_device not in self.output_connections:
-            self.output_connections.append(output_device)
-        if self not in output_device.input_connections:
-            output_device.input_connections.append(self)
 
-    def disconnect_from(self, output_device):
-        if output_device in self.output_connections:
-            self.output_connections.remove(output_device)
-        if self in output_device.input_connections:
-            output_device.input_connections.remove(self)
+    def get_connection_arguments(self, output_device, bus_name=None):
+        return [
+            # Adds or discards target device [1] from source device output bus [0]
+            (self.connect.output[bus_name], output_device),
+            # Adds or discards source device [1] from target device input bus [0]
+            (output_device.connect.input[bus_name], self),
+            # Adds or discards bus name [1] from target device 'source device -> in bus' map [0]
+            (output_device.device_map.input[self], bus_name),
+            # Adds or discards bus name [1] from source device 'target device -> in bus' map [0]
+            (self.device_map.output[output_device], bus_name)
+        ]
+    
+    def connect_to(self, output_device, bus_name=None):
+        args = self.get_connection_arguments(output_device, bus_name)
+        for arg in args:
+            arg[0].add(arg[1])
 
-    def get_signals(self, t):
-        return (device.evaluate(t) for device in self.input_connections)
+    def disconnect_from(self, output_device, bus_name=None):
+        args = self.get_connection_arguments(output_device, bus_name)
+        for arg in args:
+            arg[0].discard(arg[1])
+
+    def get_signals(self, bus_name=None):
+        return (device.evaluate(bus_name) for device in self.connect.input[bus_name])
     
     def toggle_freeze(self):
         if self.state == DeviceState.FROZEN:
@@ -69,7 +98,8 @@ class Device:
         else:
             self.state = DeviceState.FROZEN
             print("I am frozen")
-        
+    
+
 class Generator(Device):
     def __init__(self, generator_func=constant):
         super().__init__()
@@ -78,8 +108,8 @@ class Generator(Device):
     def _update(self, dt):
         return
 
-    def _evaluate(self, t):
-        return self.generator(t)
+    def _evaluate(self, bus_name=None):
+        return self.generator() if self.connect.output[bus_name] else 0.0
     
 
 class Impulse(Device):
@@ -89,9 +119,9 @@ class Impulse(Device):
         self._remaining = 0.0
         self.retrigger = retrigger
 
-    def _evaluate(self, t):
+    def _evaluate(self, bus_name=None):
         factor = self._remaining if self._remaining > 0 else 0
-        return self.signal_combiner(self.get_signals(t)) * factor
+        return self.signal_combiner(self.get_signals()) * factor
 
     def _update(self, dt):
         if self.retrigger and self._remaining <= 0:
@@ -107,8 +137,8 @@ class Channel(Device):
     def __init__(self):
         super().__init__()
 
-    def _evaluate(self, t):
-        return self.signal_combiner(self.get_signals(t))
+    def _evaluate(self, bus_name=None):
+        return self.signal_combiner(self.get_signals(bus_name))
 
     def _update(self, dt):
         pass  # If no update logic needed
@@ -116,24 +146,19 @@ class Channel(Device):
 class Mixer(Device):
     def __init__(self):
         super().__init__()
+        self.fader = {}
         self.bus = {}
-    
-    def set_bus(self, name, gain=1.0):
-        if self.not_updateable():
-            return
-        self.bus[name] = gain
 
-    def remove_bus(self, name):
-        if self.not_updateable():
-            return
-        self.bus.pop(name, None)
+    def set_bus(self, bus_name, gain=1.0):
+        self.bus[bus_name] = gain
 
-    def evaluate_bus(self, name, t):
-        factor = self.bus[name] if name in self.bus else 0.0
-        return clamp(super().evaluate(t) * factor)
+    def evaluate_bus(self, bus_name):
+        if bus_name not in self.bus:
+            return 0.0
+        return self.evaluate() * self.bus[bus_name]
 
-    def _evaluate(self, t):
-        return self.signal_combiner(self.get_signals(t))
+    def _evaluate(self, bus_name = None):
+        return self.signal_combiner(self.get_signals())
 
     def _update(self, dt):
         pass  # If no update logic needed
--- devicemanager.py
+++ devicemanager.py
@@ -6,33 +6,32 @@ from constants import (
 
 
 updatables = [
-    generator := Generator(constant(PLAYER_SHOT_FLASH_INTENSITY)),
-    impulse := Impulse(PLAYER_SHOT_FLASH_LENGTH),
-    channel := Channel(),
-     # peak detection, strongest signal is transmitted
-    mixer := Mixer()
+    generator_player_shot_flash := Generator(constant(PLAYER_SHOT_FLASH_INTENSITY)),
+    impulse_player_shot_flash := Impulse(PLAYER_SHOT_FLASH_LENGTH),
+    channel_player_shot_flash := Channel(),
+    mixer_player_shot_flash := Mixer()
 ]
-mixer.set_bus('asteroid_player_shot_flash')
-mixer.set_bus('player_player_shot_flash')
-mixer.set_bus('background_player_shot_flash', 1/64)
-mixer.set_bus('star_player_shot_flash', 1/64)
-channel.signal_combiner = max
-generator.connect_to(impulse)
-impulse.connect_to(channel)
-channel.connect_to(mixer)
+mixer_player_shot_flash.set_bus('asteroid')
+mixer_player_shot_flash.set_bus('player')
+mixer_player_shot_flash.set_bus('background', 1/64)
+mixer_player_shot_flash.set_bus('star', 1/64)
+channel_player_shot_flash.signal_combiner = max
+generator_player_shot_flash.connect_to(impulse_player_shot_flash)
+impulse_player_shot_flash.connect_to(channel_player_shot_flash)
+channel_player_shot_flash.connect_to(mixer_player_shot_flash)
 
 def hello(dt):
     for updatable in updatables:
         updatable.update(dt)
     print(
-        mixer.evaluate_bus('asteroid_player_shot_flash', 0),
-        mixer.evaluate_bus('player_player_shot_flash', 0),
-        mixer.evaluate_bus('background_player_shot_flash', 0),
-        mixer.evaluate_bus('star_player_shot_flash', 0)
+        mixer_player_shot_flash.evaluate_bus('asteroid'),
+        mixer_player_shot_flash.evaluate_bus('player'),
+        mixer_player_shot_flash.evaluate_bus('background'),
+        mixer_player_shot_flash.evaluate_bus('star')
     )
 
 def retrigger():
-    impulse.reset()
+    impulse_player_shot_flash.reset()
 
 def freeze():
-    impulse.toggle_freeze()
+    impulse_player_shot_flash.toggle_freeze()
